4fbdf781ffa6bf32f688a9ede5d4e228e35a0884
Bojin Li <bojinxx@gmail.com>
2024-02-25 15:45:12 +1100

    feat(socketio-demo): BW-30 initialize socket.io for both the frontend and backend by creating a ping-pong demo (#7)
    * feat(socketio-demo): implement a socket.io ping-pong demo
    
    * refactor(client socketio): improve type import for Socket from socket.io-client

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,27 @@
-const run = () => {
-  console.log("Server is running");
-};
+import express from "express";
+import { Server, Socket } from "socket.io";
+import { createServer } from "node:http";
 
-run();
+const app = express();
+const server = createServer(app);
+const sio = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+sio.on("connection", (socket: Socket) => {
+  console.log("New connection", socket.id);
+  socket.on("ping", (data) => {
+    console.log("Ping", `${data} from ${socket.id}`);
+    socket.emit("pong", "Pong");
+  });
+  socket.on("disconnect", () => {
+    console.log("Disconnected", socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
