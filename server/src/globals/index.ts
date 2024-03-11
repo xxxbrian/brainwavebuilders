20a99eacad9257a5a2c9a4e0dc64828996ce7346
Lincoln Yan <z5423219@unsw.edu.au>
2024-03-11 23:14:47 +1100

    fix: fix up here and there and got it working!

--- server/src/globals/index.ts
+++ server/src/globals/index.ts
@@ -1,7 +1,4 @@
 import express from "express";
-import { createServer } from "node:http";
 
 export const app = express();
 app.use(express.json());
-
-export const server = createServer(app);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
-import { server } from "./globals";
+import { app } from "./globals";
+import "./apis";
 
-server.listen(3000, () => {
+app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
