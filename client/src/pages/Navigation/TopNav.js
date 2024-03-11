c8cd41117d1b7903eda8ee7bbd152d1e2099d8fd
z5308765 <e.reinhard@student.unsw.edu.au>
2024-03-11 13:44:20 +1100

    feat(devops): BW-144 Mock Navigation Complete

--- client/src/pages/Navigation/TopNav.js
+++ client/src/pages/Navigation/TopNav.js
@@ -4,6 +4,38 @@ const handleCreateClass = () => {};
 
 const handleJoinClass = () => {};
 
+const handleNotificationsPopup = () => {};
+
+function TopNavDashboard() {
+  return (
+    <>
+      <div id="top-nav-dashboard">
+        <div id="top-nav-welcome">
+          <p id="top-nav-page-type">Dashboard</p>
+          <p id="top-nav-welcome-msg">Welcome to Brainwaves!</p>
+        </div>
+        <div id="top-nav-buttons">
+          <button onClick={handleCreateClass}>Create Class</button>
+          <button onClick={handleJoinClass}>Join Class</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function TopNavCourse() {
+  return (
+    <>
+      <div id="top-nav-course">
+        <img src="/Course_Logo.png" />
+        <p id="top-nav-course-code">TEST0000</p>
+        <p>:</p>
+        <p id="top-nav-course-name">Testing Course about Testing</p>
+      </div>
+    </>
+  );
+}
+
 function TopNav() {
   return (
     <>
@@ -11,15 +43,14 @@ function TopNav() {
         <div class="top-nav-logo">
           <img src="/Logo.jpg" />
         </div>
-        <div id="top-nav-welcome">
-          <p id="top-nav-page-type">Default</p>
-          <p id="top-nav-welcome-msg">
-            Please Log in for your unique welcome message!
-          </p>
-        </div>
-        <div id="top-nav-buttons">
-          <button onClick={handleCreateClass}>Create Class</button>
-          <button onClick={handleJoinClass}>Join Class</button>
+        {TopNavDashboard()}
+        <div id="top-nav-personal">
+          <a href="#">
+            <img src="/Notifications.png" id="top-nav-notifications" />
+          </a>
+          <a href="#">
+            <img src="/Default_Avatar.jpg" id="top-nav-avatar" />
+          </a>
         </div>
       </div>
     </>
