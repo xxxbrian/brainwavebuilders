4a5af424e92e081901e70d5131126960cad0c142
Lincoln Yan <z5423219@unsw.edu.au>
2024-03-11 22:58:08 +1100

    chore: added ping/pong example

--- server/src/handlers/ping.ts
+++ server/src/handlers/ping.ts
@@ -1,1 +1,5 @@
-export const ping = () => {};
+import { PingRequest, PingResponse } from "@/apis";
+
+export const ping = async ({ seq }: PingRequest): Promise<PingResponse> => {
+  return { seq: seq + 1 };
+};
