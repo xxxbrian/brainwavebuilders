da036ce1c51ac3b0f3e439c1f9bb3234c77d2b69
Lincoln Yan <z5423219@unsw.edu.au>
2024-03-12 23:49:50 +1100

    chore: disable lots of unnecessary rules

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -15,6 +15,10 @@ const config = {
     // Feel free to reconfigure them to your own preference.
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "no-empty-function": "off",
+    "@typescript-eslint/no-empty-function": "off",
 
     "@typescript-eslint/consistent-type-imports": [
       "warn",
