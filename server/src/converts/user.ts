d9158481697cc5dcc34fa573ab6d1c3b8de70285
Cicy Zhou <cicy.zhou7@gmail.com>
2024-03-21 22:11:33 +1100

    feat: more field in convert api

--- server/src/converts/user.ts
+++ server/src/converts/user.ts
@@ -6,5 +6,10 @@ export const userDBToAPI = (user: UserDB): UserAPI => {
     email: user.email,
     firstName: user.firstName,
     lastName: user.lastName,
+    // TODO: RPC current not support string | null
+    avatar: user.avatar ? user.avatar : undefined,
+    gender: user.gender ? user.gender : undefined,
+    title: user.title ? user.title : undefined,
+    bio: user.bio ? user.bio : undefined,
   };
 };
