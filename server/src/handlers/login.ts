055806cdb9ba60c506982665e37f97c45f6f600f
Bojin Li <bojinxx@gmail.com>
2024-03-21 19:31:17 +1100

    fix(login backend): BW-130 api user type unconverted (#21)
    fix(login backend): api user type unconvert

--- server/src/handlers/login.ts
+++ server/src/handlers/login.ts
@@ -1,4 +1,5 @@
 import { LoginRequest, LoginResponse } from "@/apis";
+import { userDBToAPI } from "@/converts/user";
 import { checkPassword, generateToken } from "@/data/auth";
 
 // login implements the login endpoint.
@@ -8,5 +9,8 @@ import { checkPassword, generateToken } from "@/data/auth";
 export const login = async (request: LoginRequest): Promise<LoginResponse> => {
   const user = await checkPassword(request.email, request.password);
   const token = await generateToken(user);
-  return { user, token };
+  return {
+    user: userDBToAPI(user),
+    token,
+  };
 };
