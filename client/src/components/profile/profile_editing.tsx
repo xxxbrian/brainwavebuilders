86c7a30d06d4bef80e6b9ce136b45c5e403fd52e
Steve <steve.yang@student.unsw.edu.au>
2024-03-22 17:57:04 +1100

    feat: implement basic edit preference

--- client/src/components/profile/profile_editing.tsx
+++ client/src/components/profile/profile_editing.tsx
@@ -1,4 +1,5 @@
 import * as Tabs from "@radix-ui/react-tabs";
+import * as Switch from "@radix-ui/react-switch";
 import React, { useState } from "react";
 import { IoMdPerson } from "react-icons/io";
 
@@ -11,13 +12,29 @@ export type ProfileProps = {
   bio?: string;
 };
 
-const ProfileEditing: React.FC<ProfileProps> = (props: ProfileProps) => {
+export type PreferenceProps = {
+  lang?: string;
+  timezone?: string;
+  notification: boolean;
+  recommendation: boolean;
+};
+
+const ProfileEditing: React.FC<ProfileProps> = (
+  props: ProfileProps,
+  prefernce: PreferenceProps,
+) => {
   const [firstName, setFirstName] = useState(props.firstName);
   const [lastName, setLastName] = useState(props.lastName);
   const [title, setTitle] = useState(props.title ?? "");
   const [gender, setGender] = useState(props.gender ?? "");
   const [bio, setBio] = useState(props.bio ?? "");
   const [avatar, setAvatar] = useState(props.avatar);
+  const [lang, setLang] = useState(prefernce.lang);
+  const [timezone, setTimezone] = useState(prefernce.timezone);
+  const [notification, setNotification] = useState(prefernce.notification);
+  const [recommendation, setRecommendation] = useState(
+    prefernce.recommendation,
+  );
 
   const handleAvatarChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files?.[0]) {
@@ -49,9 +66,28 @@ const ProfileEditing: React.FC<ProfileProps> = (props: ProfileProps) => {
     setBio(value);
   };
 
+  const onChangeLang = (value: string) => {
+    setLang(value);
+  };
+
+  const onChangeTimezone = (value: string) => {
+    setTimezone(value);
+  };
+
+  const onChangeNotification = (value: boolean) => {
+    setNotification(value);
+  };
+
+  const onChangeRecommendation = (value: boolean) => {
+    setRecommendation(value);
+  };
+
   const onClickSaveProfile = () => {
     // TODO
-    console.log("Saving profile with:", { firstName, lastName });
+  };
+
+  const onClickSavePreferences = () => {
+    // TODO
   };
 
   return (
@@ -67,7 +103,7 @@ const ProfileEditing: React.FC<ProfileProps> = (props: ProfileProps) => {
           Edit Profile
         </Tabs.Trigger>
         <Tabs.Trigger
-          value="preference"
+          value="preferences"
           className="px-4 py-2 text-xl font-medium text-blue-800
                      focus:outline-none
                      hover:border-b-blue-500 hover:text-blue-500
@@ -125,7 +161,7 @@ const ProfileEditing: React.FC<ProfileProps> = (props: ProfileProps) => {
                 type="text"
                 id="firstName"
                 name="firstName"
-                value={props.firstName}
+                value={firstName}
                 onChange={(e) => onChangeFirstName(e.target.value)}
                 className="mt-1 block w-full border-2 sm:text-xl border-gray-300 rounded-lg p-2 focus:outline-none focus:border-blue-500 focus:border-2"
               />
@@ -140,7 +176,7 @@ const ProfileEditing: React.FC<ProfileProps> = (props: ProfileProps) => {
                 type="text"
                 id="lastName"
                 name="lastName"
-                value={props.lastName}
+                value={lastName}
                 onChange={(e) => onChangeLastName(e.target.value)}
                 className="mt-1 block w-full border-2 sm:text-xl border-gray-300 rounded-lg p-2 focus:outline-none focus:border-blue-500 focus:border-2"
               />
@@ -213,8 +249,94 @@ const ProfileEditing: React.FC<ProfileProps> = (props: ProfileProps) => {
         </div>
       </Tabs.Content>
 
-      <Tabs.Content value="preferences" className="p-4 bg-white rounded-lg">
-        {/* Form to edit preferences goes here */}
+      <Tabs.Content
+        value="preferences"
+        className="flex flex-col p-4 bg-white rounded-lg space-y-8 items-center"
+      >
+        <div className="w-full">
+          <form className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            {/* Language */}
+            <div>
+              <label htmlFor="lang" className="block text-xl font-medium">
+                Preferred language
+              </label>
+              <select
+                id="lang"
+                name="lang"
+                value={lang}
+                onChange={(e) => onChangeLang(e.target.value)}
+                className="mt-1 block w-full border-2 sm:text-xl border-gray-300 rounded-lg p-3 focus:outline-none focus:border-blue-500 focus:border-2"
+              >
+                <option value="">Select your preferred language</option>
+                <option value="Mr">English</option>
+                <option value="Ms">Mandarin</option>
+                <option value="Dr">Korean</option>
+              </select>
+            </div>
+
+            {/* Timezone */}
+            <div>
+              <label htmlFor="timezone" className="block text-xl font-medium">
+                Time Zone
+              </label>
+              <select
+                id="timezone"
+                name="timezone"
+                value={timezone}
+                onChange={(e) => onChangeTimezone(e.target.value)}
+                className="mt-1 block w-full border-2 sm:text-xl border-gray-300 rounded-lg p-3 focus:outline-none focus:border-blue-500 focus:border-2"
+              >
+                <option value="">Select Time Zone</option>
+                <option value="female">
+                  Australian Eastern Standard Time (AEST)
+                </option>
+              </select>
+            </div>
+
+            <div>Notifications</div>
+
+            {/* Notifications Switch */}
+            <div className="flex items-center md:col-span-2">
+              <Switch.Root
+                checked={notification}
+                onCheckedChange={onChangeNotification}
+                className="w-[42px] h-[25px] bg-blackA6 rounded-full relative bg-zinc-300 data-[state=checked]:bg-[#004E89] cursor-default"
+                id="notification"
+              >
+                <Switch.Thumb className="block w-[20px] h-[20px] bg-white rounded-full transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[19px]" />
+              </Switch.Root>
+              <label className="text-xl ml-2" htmlFor="notification">
+                Receive notifications for announcements
+              </label>
+            </div>
+
+            {/* Recommendations Switch */}
+            <div className="flex items-center md:col-span-2">
+              <Switch.Root
+                checked={recommendation}
+                onCheckedChange={onChangeRecommendation}
+                className="w-[42px] h-[25px] bg-blackA6 rounded-full relative bg-zinc-300 data-[state=checked]:bg-[#004E89] cursor-default"
+                id="recommendation"
+              >
+                <Switch.Thumb className="block w-[20px] h-[20px] bg-white rounded-full transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[19px]" />
+              </Switch.Root>
+              <label className="text-xl ml-2" htmlFor="recommendation">
+                Get recommendations for courses
+              </label>
+            </div>
+
+            {/* Save Button */}
+            <div className="md:col-span-2 flex justify-end">
+              <button
+                type="button"
+                onClick={onClickSavePreferences}
+                className="mt-4 bg-orange-500 text-white px-6 py-2 rounded-md hover:bg-opacity-90"
+              >
+                Save
+              </button>
+            </div>
+          </form>
+        </div>
       </Tabs.Content>
 
       <Tabs.Content value="security" className="p-4 bg-white rounded-lg">
