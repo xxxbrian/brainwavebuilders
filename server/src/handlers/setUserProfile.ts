49132fe67eb2dad8f57a5389035459d13bb46c46
Bojin Li <bojinxx@gmail.com>
2024-04-02 15:00:11 +1100

    refactor: format existing code

--- server/src/handlers/setUserProfile.ts
+++ server/src/handlers/setUserProfile.ts
@@ -6,15 +6,17 @@ import { updateUserProfile } from "@/data/profile";
 // This code has been automatically generated.
 // You can move this function to other files within the /app/server/src/handlers directory,
 // as long as the signature remains the same and the function is exported.
-export const setUserProfile = async (request: SetUserProfileRequest): Promise<SetUserProfileResponse> => {
-    let { user, token } = request;
-    let askUser = await getUserByToken(token)
-    if (askUser === null) {
-        throw new Error("Invalid token");
-    }
-    if (askUser.email !== user.email) {
-        throw new Error("Invalid user");
-    }
-    await updateUserProfile(user);
-    return {};
-}
+export const setUserProfile = async (
+  request: SetUserProfileRequest,
+): Promise<SetUserProfileResponse> => {
+  let { user, token } = request;
+  let askUser = await getUserByToken(token);
+  if (askUser === null) {
+    throw new Error("Invalid token");
+  }
+  if (askUser.email !== user.email) {
+    throw new Error("Invalid user");
+  }
+  await updateUserProfile(user);
+  return {};
+};
