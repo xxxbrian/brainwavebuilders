a4e3e7742c7223829198a60b9c622a4c858c6db5
Lincoln Yan <yyjlincoln@gmail.com>
2024-04-11 00:39:11 +1000

    chore: forum WIP

--- client/src/app/course/[courseId]/forum/page.tsx
+++ client/src/app/course/[courseId]/forum/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import { useCourseFromLayout } from "../layout";
+
 export const ForumPage: React.FC = () => {
+  const course = useCourseFromLayout();
+
   return (
     <div>
-      <h1>Forum</h1>
+      <h1>Forum {course.id}</h1>
     </div>
   );
 };
