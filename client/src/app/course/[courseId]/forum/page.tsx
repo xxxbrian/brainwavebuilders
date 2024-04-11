f476af40ed08e1767ba06567cad716554e359473
Lincoln Yan <z5423219@unsw.edu.au>
2024-04-12 00:34:44 +1000

    chore: gets forum id in forum page

--- client/src/app/course/[courseId]/forum/page.tsx
+++ client/src/app/course/[courseId]/forum/page.tsx
@@ -3,13 +3,38 @@
 import { useCourse } from "@/contexts/CourseContext";
 import { WithStudentRole, WithTeacherRole } from "@/contexts/CourseRoleContext";
 import AdvancedEditor from "@/components/editor/advanced-editor";
+import { useEffect, useState } from "react";
+import { CenteredLoading } from "@/components/loading";
+import { useBackend } from "@/hooks/useBackend";
 
 export const ForumPage: React.FC = () => {
   const course = useCourse();
 
+  const [forumId, setForumId] = useState<string | null>(null);
+
+  const backend = useBackend();
+
+  useEffect(() => {
+    if (course === null) return;
+
+    const inner = async () => {
+      const { forum } = await backend.getForumByCourseID({
+        courseID: course.id,
+      });
+
+      setForumId(forum.id);
+    };
+
+    void inner();
+  }, [backend, course]);
+
+  if (forumId === null) return <CenteredLoading />;
+
   return (
     <div className="flex flex-col justify-center items-center space-y-4">
-      <h1>Forum {course.id}</h1>
+      <h1>
+        Forum CourseID={course.id} ForumID={forumId}
+      </h1>
       <WithTeacherRole>
         <p>Only teachers can see this</p>
       </WithTeacherRole>
