98eda29e42e1040715fd9ed5821f472f8a418df6
Bojin Li <bojinxx@gmail.com>
2024-04-12 14:10:53 +1000

    refactor(calendar): BW-252 update events structure to use Record instead of Map  (#85)
    * refactor(calendar): update events structure to use Record instead of Map
    
    * fix: use [] instand get method in Object

--- client/src/components/calendar/CalendarBoardMini.tsx
+++ client/src/components/calendar/CalendarBoardMini.tsx
@@ -19,9 +19,11 @@ export const CalendarBoardMini: React.FC<CalendarProps> = ({
 
   function eventsInDay(day: Date) {
     const todayEvents =
-      events.get(
-        `${day.getFullYear()}-${day.getMonth() + 1}-${day.getDate()}`,
-      ) ?? [];
+      // events.get(
+      //   `${day.getFullYear()}-${day.getMonth() + 1}-${day.getDate()}`,
+      // ) ?? [];
+      events[`${day.getFullYear()}-${day.getMonth() + 1}-${day.getDate()}`] ??
+      [];
     return (
       <>
         {todayEvents.map((event, index) => (
