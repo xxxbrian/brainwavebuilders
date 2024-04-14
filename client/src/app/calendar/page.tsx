2ebfd93d5509b26ef87d462d30e853ff9a1c4dda
Bojin Li <bojinxx@gmail.com>
2024-04-14 19:24:53 +1000

    feat: connect calandar page with the backend data

--- client/src/app/calendar/page.tsx
+++ client/src/app/calendar/page.tsx
@@ -5,46 +5,27 @@ import {
   Calendar as CalendarComponent,
 } from "@/components/calendar/Calendar";
 import { PageFrame } from "@/components/structural/PageFrame";
+import { useBackend } from "@/hooks/useBackend";
 
-import React from "react";
+import React, { useEffect } from "react";
 
 export const Calendar: React.FC = () => {
-  const mockTime = new Date("2024-03-01");
-  const mockEvents = () => {
-    const events: Record<string, Event[]> = {};
-    for (let i = 1; i <= 31; i++) {
-      const key = `${mockTime.getFullYear()}-${mockTime.getMonth() + 1}-${i}`;
-      if (i % 9 === 0) {
-        events[key] = [
-          {
-            name: "CS3900 Meeting",
-            time: "3 PM - 5 PM",
-            // random in ["lecture", "tutorial", "assignment", "exam"]
-            type:
-              ["lecture", "tutorial", "assignment", "exam"].sort(
-                () => Math.random() - 0.5,
-              )[0] ?? "assignment",
-          },
-          {
-            name: "Assignment 1 Due",
-            time: "10 AM - 11 AM",
-            type:
-              ["lecture", "tutorial", "assignment", "exam"].sort(
-                () => Math.random() - 0.5,
-              )[0] ?? "exam",
-          },
-        ];
-      } else {
-        events[key] = [];
-      }
-    }
-    return events;
-  };
+  const [events, setEvents] = React.useState<Record<string, Event[]>>({});
+
+  const backend = useBackend();
+
+  useEffect(() => {
+    const inner = async () => {
+      const { events } = await backend.getUserEvents({});
+      setEvents(events);
+    };
+    void inner();
+  }, [backend]);
 
   return (
     <PageFrame title="Calendar" standardWidth={false}>
       <div className="flex flex-col gap-4">
-        <CalendarComponent today={mockTime} events={mockEvents()} />
+        <CalendarComponent today={new Date()} events={events} />
       </div>
     </PageFrame>
   );
