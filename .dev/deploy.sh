e997cb8e342682e645986adfee7bb71050f11b41
Lincoln Yan <z5423219@unsw.edu.au>
2024-04-17 02:37:48 +1000

    fix: prune image before deployment

--- .dev/deploy.sh
+++ .dev/deploy.sh
@@ -3,5 +3,8 @@
 set -ex
 
 cd $(dirname $0)/../
+docker image prune -f
+
+set +x
 env $(cat .dev/secrets/production.env) docker compose -f docker-compose.deploy.yaml pull
 env $(cat .dev/secrets/production.env) docker compose -f docker-compose.deploy.yaml up -d
